28af8e9d379b8a0f67aadf38750c3702c0a595c8
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-10-05 18:00:06 +0200

    configuring bottom tabs icons, styles

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MyTabs from "./components/MyTabs";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="light" />
       <MyTabs />
     </NavigationContainer>
   );
--- components/MyTabs.js
+++ components/MyTabs.js
@@ -1,14 +1,35 @@
 import HomeScreen from "../screens/HomeScreen";
 import SettingsScreen from "../screens/SettingsScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const BottomTab = createBottomTabNavigator();
 
 export default function MyTabs() {
   return (
-    <BottomTab.Navigator>
-      <BottomTab.Screen name="Home" component={HomeScreen} />
-      <BottomTab.Screen name="Settings" component={SettingsScreen} />
+    <BottomTab.Navigator screenOptions={{
+        headerStyle: { backgroundColor: '#3c0a6b' },
+        headerTintColor: 'white',
+        tabBarActiveTintColor: '#3c0a6b'
+    }}>
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" color={color} size={size} />
+          ),
+        }}
+      />
     </BottomTab.Navigator>
   );
 }
